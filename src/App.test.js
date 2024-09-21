f36b69e969f36abbd2eb9ecb6c3ab0d676fa46b9
Zigalow <s224302@dtu.dk>
2024-09-21 21:04:42 +0200

    Fixed issue

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    render(<App/>);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
 });
+
+/*test("new test", () => {
+    let usernameField = screen.getByLabelText("username");
+    expect(usernameField).toHaveAttribute(
+        "placeholder", "indtast brugernavn")
+})*/
